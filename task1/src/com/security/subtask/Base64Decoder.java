60c3fbb3c5e9a8ec139c7d143d04c9ac54300a7f
Pavlo Pankratov <p.p.1999027@gmail.com>
2020-11-29 18:07:57 +0200

    Added base64 decoder impl

---
+++ task1/src/com/security/subtask/Base64Decoder.java
@@ -0,0 +1,44 @@
+package com.security.subtask;
+
+import java.util.stream.Stream;
+
+import static com.security.subtask.Base64Utils.BASE_64_CHART;
+
+public class Base64Decoder {
+    public final String decode(final String cipher) {
+        final String binary = getBinaryStringFromCipher(cipher);
+        final StringBuilder result = new StringBuilder();
+        final StringBuilder binaryBuilder = new StringBuilder(binary);
+
+        for (int i = 0; i < binary.length() / 8; i++) {
+            String bin = binaryBuilder.substring(0, 8);
+            binaryBuilder.delete(0, 8);
+            int decimal = Integer.parseInt(bin, 2);
+            result.append((char) decimal);
+        }
+
+        return result.toString();
+    }
+
+    private String getBinaryStringFromCipher(String cipher) {
+        return Stream.of(cipher.split(""))
+                .map(letter -> BASE_64_CHART.get(letter))
+                .map(this::getBinary)
+                .reduce("", (s1, s2) -> s1 + s2);
+    }
+
+    private String getBinary(final Integer number) {
+        if (number == 64){
+            return "00";
+        }
+        final StringBuilder binary = new StringBuilder(Integer.toString(number, 2));
+        int length = binary.length();
+        if (length != 6) {
+            for (int i = 0; i < 6 - length; i++) {
+                binary.insert(0, "0");
+            }
+        }
+        return binary.toString();
+    }
+}
+
