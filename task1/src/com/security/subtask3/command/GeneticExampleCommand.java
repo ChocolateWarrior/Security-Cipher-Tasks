3138ff712307f3c961cca60dc433dde5d8302800
ChocolateWarrior <markzubach13@gmail.com>
2020-12-06 18:15:57 +0200

    init new branch subtask3-genetic

---
+++ task1/src/com/security/subtask3/command/GeneticExampleCommand.java
@@ -0,0 +1,9 @@
+package com.security.subtask3.command;
+
+//Write a code to attack some simple substitution cipher. To reduce the complexity of this one we will use only
+// uppercase letters, so the keyspace is only 26! To get this one right automatically you will probably need to use some
+// sort of genetic algorithm (which worked the best last year), simulated annealing or gradient descent. Seriously,
+// write it right now, you will need it to decipher the next one as well. Bear in mind,
+// thereﾖʡ+ 7 (A=SVxH,PbW<O+7O4EV;OwD;U=NwDWia/!
+public class GeneticExampleCommand {
+}
