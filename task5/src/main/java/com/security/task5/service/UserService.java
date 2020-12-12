51a4d688ac766fa5b80888d3db9cde5995c68b59
ChocolateWarrior <markzubach13@gmail.com>
2020-12-12 23:22:24 +0200

    minor: UserService refactor, added Constants interface

--- task5/src/main/java/com/security/task5/service/UserService.java
+++ task5/src/main/java/com/security/task5/service/UserService.java
@@ -9,13 +9,15 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static com.security.task5.utils.Constants.*;
+
 @Log4j2
 @Service
-public class
-UserService {
+public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -37,48 +39,56 @@ UserService {
     public String login(final UserDTO userDTO) {
         final User user = userRepository.findByLogin(userDTO.getLogin());
         final String code = secretKeyGen.getTOTPCode(user.getKey());
-        if (user == null || !passwordEncoder.matches(userDTO.getPassword(), user.getPassword())
+        if (Objects.isNull(user) || !passwordEncoder.matches(userDTO.getPassword(), user.getPassword())
                 || !code.equals(userDTO.getNumber())) {
-            return "login failed!";
+            return LOGIN_FAILED;
         }
         if (user.getCompromised()){
-            final String key = secretKeyGen.generateSecretKey();
-            user.setCompromised(false);
-            user.setKey(key);
-            userRepository.save(user);
-            return "You was compromised. Your new secret key is: " + key + ". Please reset your password";
+            final String key = markUserCompromised(user);
+            return YOU_WERE_COMPROMISED + key + PLEASE_RESET_YOUR_PASSWORD;
         }
-        return "login success!";
+        return LOGIN_SUCCESS;
     }
 
-    public String saveNewUser(final UserDTO userdto) {
-        final User userFromDb = userRepository.findByLogin(userdto.getLogin());
+    private String markUserCompromised(User user) {
+        final String key = secretKeyGen.generateSecretKey();
+        user.setCompromised(false);
+        user.setKey(key);
+        userRepository.save(user);
+        return key;
+    }
+
+    public String saveNewUser(final UserDTO userDto) {
+        final User userFromDb = userRepository.findByLogin(userDto.getLogin());
 
         if (userFromDb != null) {
-            log.warn("login not unique!");
-            return "login not unique!";
+            log.warn(LOGIN_NOT_UNIQUE);
+            return LOGIN_NOT_UNIQUE;
         }
-        final String userPassword = userdto.getPassword();
+        final String userPassword = userDto.getPassword();
         final Matcher pwMatcher = pwPattern.matcher(userPassword);
 
         if (!pwMatcher.matches()) {
-            log.warn("password is not secure!");
-            return "password is not secure!";
+            log.warn(PASSWORD_IS_NOT_SECURE);
+            return PASSWORD_IS_NOT_SECURE;
         }
 
         final String hash = passwordEncoder.encode(userPassword);
         final String key = secretKeyGen.generateSecretKey();
-        final User user = User
-                .builder()
-                .login(userdto.getLogin())
-                .password(hash)
-                .compromised(false)
-                .version(env.getProperty("encryption.version"))
-                .key(key)
-                .build();
+        final User user = buildUser(userDto, hash, key);
 
         userRepository.save(user);
-        log.info("User was saved. Username : " + user.getLogin());
+        log.info(USER_WAS_SAVED_USERNAME + user.getLogin());
         return key;
+    }
+
+    private User buildUser(UserDTO userDto, String hash, String key) {
+        return User.builder()
+                .login(userDto.getLogin())
+                .password(hash)
+                .compromised(false)
+                .version(env.getProperty(ENCRYPTION_VERSION))
+                .key(key)
+                .build();
     }
 }
---
+++ task5/src/main/java/com/security/task5/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.security.task5.utils;
+
+public interface Constants {
+
+    String LOGIN_FAILED = "login failed!";
+    String YOU_WERE_COMPROMISED = "You was compromised. Your new secret key is: ";
+    String PLEASE_RESET_YOUR_PASSWORD = ". Please reset your password";
+    String LOGIN_SUCCESS = "login success!";
+    String LOGIN_NOT_UNIQUE = "login not unique!";
+    String PASSWORD_IS_NOT_SECURE = "password is not secure!";
+    String ENCRYPTION_VERSION = "encryption.version";
+    String USER_WAS_SAVED_USERNAME = "User was saved. Username : ";
+}
